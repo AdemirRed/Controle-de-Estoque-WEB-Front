d8d3bc8b43aa9a2ed2d9ec997d6b6113efd12438
AdemirRed <ademir1de1oliveira@gmail.com>
2025-05-30 10:27:44 -0300

    remove serviço de autenticação e ajusta URL base da API para uso local

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://redblackspy.ddns.net:2001', // Não use variável de ambiente se não for necessário
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json'
   }
--- src/services/authService.js
+++
@@ -1,5 +0,0 @@
-import api from './api';
-
-export function login(dados) {
-  return api.post('/auth/sessao', dados);
-}
